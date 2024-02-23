2b06a8da1659bc756e5a7984a321cfe2d7b4596a
ShiroKuroGeo <inocgeorgealfeser@gmail.com>
2024-02-23 18:13:14 +0800

    Ready to Deploy

--- src/views/selectionView.vue
+++ src/views/selectionView.vue
@@ -72,7 +72,7 @@
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Secret Message <span class="text-uppercase">{{ number }}</span>
           </h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <button type="button" class="btn btn-close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
